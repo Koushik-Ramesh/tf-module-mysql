72fbc4dd902c69ca7aacdddab0210adc204138d7
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-03-19 16:23:16 +0530

    Autopush

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # tf-module-mysql
+
+All the codes needed to create the RDS [A Managed service for SQL on AMS] is going to be placed in this backend module and the associated root module will be 'terraform-databases' 
+
+DB's supported by MYSQL :
+
+```
+1) MariaDB
+2) SQL-server
+3) Oracle
+4) Aurora
+5) Postgress
+6) Aurora Postgress
+7) IBM DB 2
+8) Microsoft sql
+
+```
---
+++ cluster.tf
@@ -0,0 +1,28 @@
+# Creates RDS Instance
+resource "aws_db_instance" "mysql" {
+  allocated_storage    = 10
+  db_name              = "roboshop-${var.ENV}-mysql"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "admin1"
+  password             = "roboshop1"
+  parameter_group_name = aws_db_parameter_group.mysql.name
+  db_subnet_group_name = aws_db_subnet_group.mysql.name
+  skip_final_snapshot  = true
+}
+
+resource "aws_db_subnet_group" "mysql" {
+  name       = "roboshop-${var.ENV}-mysql-subnet-group"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
+
+  tags = {
+    Name = "roboshop-${var.ENV}-mysql-subnet-group"
+  }
+}
+
+resource "aws_db_parameter_group" "mysql" {
+  name   = "roboshop-${var.ENV}-mysql-parameter-group"
+  family = "mysql5.7"
+}
+
