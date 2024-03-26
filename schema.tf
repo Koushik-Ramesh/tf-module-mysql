8121899caa2e4793be78bea58c0fef2aec762339
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-03-26 13:39:33 +0530

    Autopush

--- schema.tf
+++ schema.tf
@@ -6,7 +6,7 @@ resource "null_resource" "schema" {
     curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
     unzip -o mysql.zip
     cd mysql-main
-    mysql -h connectionString ? -uadmin1 -proboshop1 < shipping.sql
+    mysql -h ${aws_db_instance.address.mysql} -uadmin1 -proboshop1 < shipping.sql
 EOF
   }
 }
