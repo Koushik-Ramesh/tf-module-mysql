dd3c1b02d77fea260b6801c96c6f84ae98e62d89
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-03-26 14:06:53 +0530

    Autopush

--- schema.tf
+++ schema.tf
@@ -2,7 +2,7 @@ resource "null_resource" "schema" {
     depends_on = [ aws_db_instance.mysql ]
     provisioner "local-exec" {
         command = <<EOF
-            cd/tmp
+            cd /tmp
             curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
             unzip mysql.zip
             cd mysql-main
